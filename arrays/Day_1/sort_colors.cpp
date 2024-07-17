6970bed55ce9fd36475a68fec263d1435813e0e1
sahilrik <sahilrik@gmail.com>
2024-07-18 01:33:49 +0530

    second question

--- arrays/Day_1/sort_colors.cpp
+++ arrays/Day_1/sort_colors.cpp
@@ -1,7 +1,29 @@
-#include<iostream>
-using namespace std;
+class Solution
+{
+public:
+    void sortColors(vector<int> &nums)
+    {
+        int l = 0;
+        int r = nums.size() - 1;
 
-int main(){
-    cout<<"problem encounter"<<endl;
-    return 0;
-}
+        int i = 0;
+        while (i <= r)
+        {
+            if (nums[i] == 0)
+            {
+                swap(nums[i], nums[l++]);
+                ++i;
+            }
+            else if (nums[i] == 2)
+            {
+                swap(nums[i], nums[r--]);
+            }
+            else
+            {
+                ++i;
+            }
+        }
+    }
+};
+
+// When the current element is 2, and you swap it with the element at the r pointer, you should not increment i immediately. This is because the element that gets swapped into the current position (i.e., nums[i]) has not been processed yet. It could be a 0, 1, or another 2, and you need to check and handle it accordingly.
